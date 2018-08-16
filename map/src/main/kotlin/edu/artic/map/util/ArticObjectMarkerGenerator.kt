52b5205a594584e1594ebfcb739196149560f228
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-08-16 12:03:49 -0400

    [Map] - Images: restrict ArticObject bitmaps to software layer

--- map/src/main/kotlin/edu/artic/map/util/ArticObjectMarkerGenerator.kt
+++ map/src/main/kotlin/edu/artic/map/util/ArticObjectMarkerGenerator.kt
@@ -4,20 +4,32 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import de.hdodenhof.circleimageview.CircleImageView
+import android.widget.ImageView
 import edu.artic.map.R
 
 
 class ArticObjectMarkerGenerator(context: Context) : BaseMarkerGenerator(context) {
 
+    /**
+     * Where the [icon][BaseMarkerGenerator.makeIcon] will appear.
+     */
+    private val target: ImageView
+
     init {
         container = LayoutInflater.from(context)
                 .inflate(R.layout.marker_artic_object, null) as ViewGroup
+        target = container.findViewById(R.id.circularImage)
     }
 
 
+    /**
+     * NB: 'imageViewBitmap' _MUST_ be rendered in software. If its config
+     * is [android.graphics.Bitmap.Config.HARDWARE], the
+     * [CircleImageView][de.hdodenhof.circleimageview.CircleImageView]
+     * may crash.
+     */
     fun makeIcon(imageViewBitmap: Bitmap): Bitmap {
-        container.findViewById<CircleImageView>(R.id.circularImage).setImageBitmap(imageViewBitmap)
+        target.setImageBitmap(imageViewBitmap)
         return makeIcon()
     }
 }
